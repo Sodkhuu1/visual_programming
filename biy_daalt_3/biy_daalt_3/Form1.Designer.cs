27eb56d6ebc508fb403783e58fdc44b09e0d1716
B222270821 <b222270821@must.edu.mn>
2025-12-03 17:43:14 +0800

    4th task of assignment

---
+++ biy_daalt_3/biy_daalt_3/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace biy_daalt_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            canvasPanel = new Panel();
+            rbLine = new RadioButton();
+            rbRect = new RadioButton();
+            rbEllipse = new RadioButton();
+            btnClear = new Button();
+            SuspendLayout();
+            // 
+            // canvasPanel
+            // 
+            canvasPanel.BackColor = SystemColors.ButtonHighlight;
+            canvasPanel.Location = new Point(12, 128);
+            canvasPanel.Name = "canvasPanel";
+            canvasPanel.Size = new Size(776, 310);
+            canvasPanel.TabIndex = 0;
+            canvasPanel.MouseDown += canvasPanel_MouseDown;
+            canvasPanel.MouseUp += canvasPanel_MouseUp;
+            // 
+            // rbLine
+            // 
+            rbLine.AutoSize = true;
+            rbLine.Location = new Point(89, 46);
+            rbLine.Name = "rbLine";
+            rbLine.Size = new Size(82, 24);
+            rbLine.TabIndex = 1;
+            rbLine.TabStop = true;
+            rbLine.Text = "Шулуун";
+            rbLine.UseVisualStyleBackColor = true;
+            rbLine.CheckedChanged += rbLine_CheckedChanged;
+            // 
+            // rbRect
+            // 
+            rbRect.AutoSize = true;
+            rbRect.Location = new Point(291, 46);
+            rbRect.Name = "rbRect";
+            rbRect.Size = new Size(112, 24);
+            rbRect.TabIndex = 2;
+            rbRect.TabStop = true;
+            rbRect.Text = "Дөрвөлжин";
+            rbRect.UseVisualStyleBackColor = true;
+            rbRect.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // rbEllipse
+            // 
+            rbEllipse.AutoSize = true;
+            rbEllipse.Location = new Point(471, 46);
+            rbEllipse.Name = "rbEllipse";
+            rbEllipse.Size = new Size(69, 24);
+            rbEllipse.TabIndex = 3;
+            rbEllipse.TabStop = true;
+            rbEllipse.Text = "Дугуй";
+            rbEllipse.UseVisualStyleBackColor = true;
+            rbEllipse.CheckedChanged += rbEllipse_CheckedChanged;
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(663, 44);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(94, 29);
+            btnClear.TabIndex = 4;
+            btnClear.Text = "Арилгах";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnClear);
+            Controls.Add(rbEllipse);
+            Controls.Add(rbRect);
+            Controls.Add(rbLine);
+            Controls.Add(canvasPanel);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private RadioButton rbLine;
+        private RadioButton rbRect;
+        private RadioButton rbEllipse;
+        private Button btnClear;
+        private Panel canvasPanel;
+    }
+}
---
+++ biy_daalt_3/biy_daalt_3/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+namespace biy_daalt_3
+{
+    public partial class Form1 : Form
+    {
+        private bool isDrawing = false;
+        private Point startPoint;
+        private Bitmap canvasBitmap;
+        private Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            canvasBitmap = new Bitmap(canvasPanel.Width, canvasPanel.Height);
+            g = Graphics.FromImage(canvasBitmap);
+            g.Clear(Color.White);
+
+            canvasPanel.BackgroundImage = canvasBitmap;
+            canvasPanel.BackgroundImageLayout = ImageLayout.None;
+
+            rbLine.Checked = true;
+        }
+
+        private void canvasPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDrawing = true;
+            startPoint = e.Location;
+        }
+
+        private void canvasPanel_MouseUp(Object sender, MouseEventArgs e)
+        {
+            if (!isDrawing) return;
+            isDrawing = false;
+
+            Point endPoint = e.Location;
+
+            Pen pen = new Pen(Color.Black, 2);
+            if (rbLine.Checked)
+            {
+                g.DrawLine(pen, startPoint, endPoint);
+            }
+            else
+            {
+                Rectangle rect = new Rectangle(
+                    Math.Min(startPoint.X, endPoint.X),
+                    Math.Min(startPoint.Y, endPoint.Y),
+                    Math.Abs(endPoint.X - startPoint.X),
+                    Math.Abs(endPoint.Y - startPoint.Y));
+                if (rbRect.Checked)
+                    g.DrawRectangle(pen, rect);
+                else if (rbEllipse.Checked)
+                    g.DrawEllipse(pen, rect);
+            }
+            pen.Dispose();
+            canvasPanel.Invalidate();
+        }
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rbLine_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rbEllipse_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            canvasPanel.Invalidate();
+        }
+    }
+}
